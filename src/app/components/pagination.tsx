aa47bb1a36e5e71d0bbfe5df579291c2f9c74122
jaki <jaki@MacBook-Pro-4.local>
2025-04-12 17:13:08 -0400

    upate pagination to use icons

--- src/app/components/pagination.tsx
+++ src/app/components/pagination.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { PrevIcon, NextIcon } from "./icons";
+
 export interface PaginationMeta {
   currentPage: number;
   pageSize: number;
@@ -81,9 +83,7 @@ export default function Pagination({ paginationMeta, onPageChange, searchTerm }:
               }`}
             >
               <span className="sr-only">Previous</span>
-              <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
-              </svg>
+              <PrevIcon />
             </button>
             
             {/* Page Numbers */}
@@ -129,9 +129,7 @@ export default function Pagination({ paginationMeta, onPageChange, searchTerm }:
               }`}
             >
               <span className="sr-only">Next</span>
-              <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-              </svg>
+              <NextIcon />
             </button>
           </nav>
         </div>
