5623de463ec53923f2039a9de2edd498f0e41774
jaki <jaki@MacBook-Pro-4.local>
2025-04-12 17:11:32 -0400

    chore: install and run prettier

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,12 @@
-const Footer = () => (         <footer className="bg-primary-dark text-white py-6">
+const Footer = () => (
+  <footer className="bg-primary-dark text-white py-6">
     <div className="max-w-full mx-auto px-8 text-center">
-      <p className="text-sm">© {new Date().getFullYear()} Solace Advocates Platform. All rights reserved.</p>
+      <p className="text-sm">
+        © {new Date().getFullYear()} Solace Advocates Platform. All rights
+        reserved.
+      </p>
     </div>
-  </footer>)
+  </footer>
+);
 
 export default Footer;
