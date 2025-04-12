7f9a77c6bdcb49258e37aae03e759e55ccf5c29d
jaki <jaki@MacBook-Pro-4.local>
2025-04-12 19:17:08 -0400

    fix: update migration script to use dotenv

--- src/db/migrate.js
+++ src/db/migrate.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const { drizzle } = require("drizzle-orm/postgres-js");
 const { migrate } = require("drizzle-orm/postgres-js/migrator");
 const postgres = require("postgres");
+
 
 const runMigration = async () => {
   if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
